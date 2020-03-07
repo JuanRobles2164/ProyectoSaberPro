7ae67bde3629d2d233092e2eafdb7f423f7c5f4d
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-03-07 00:47:19 -0500

    Casi completado el 3er requisito

--- ProyectoSaberPro/Controllers/HomeController.cs
+++ ProyectoSaberPro/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProyectoSaberPro.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,20 @@ namespace ProyectoSaberPro.Controllers
 {
     public class HomeController : Controller
     {
-
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            var userList = db.Users.ToList();
+            if (userList.Count == 0)
+            {
+                //Aquí debería retornar a la vista de registro del primer primerísimo admin
+                //Es básicamente la misma de todas xd pero se registra como admin
+                //Sino entiende, es lo del requisito 3 del correo de Laura
+                return View();
+            } else {
+                return View();
+            }
+            
         }
 
         public ActionResult About()
