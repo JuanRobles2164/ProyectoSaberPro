7beb54658033eaaae382abe6b2832adff0840010
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-03-09 00:24:16 -0500

    Redireccionamiento después del registro del primer admin

--- ProyectoSaberPro/Controllers/AccountController.cs
+++ ProyectoSaberPro/Controllers/AccountController.cs
@@ -168,7 +168,7 @@ namespace ProyectoSaberPro.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirmar cuenta", "Para confirmar la cuenta, haga clic <a href=\"" + callbackUrl + "\">aquí</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Administrador");
                 }
                 AddErrors(result);
             }
